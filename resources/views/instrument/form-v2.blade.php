467597c420568f4f468e901cb2cd82da175b444f
fawazbayureksa <fawwazbayureksa@gmail.com>
2026-02-21 13:01:11 +0700

    enhacment

--- resources/views/instrument/form-v2.blade.php
+++ resources/views/instrument/form-v2.blade.php
@@ -202,45 +202,6 @@
                                     <div class="invalid-feedback d-block">{{ $message }}</div>
                                 @enderror
                             </div>
-                            <div class="col-md-4">
-                                <label class="form-label">Bidang Keahlian</label>
-                                <select name="expertise" id="expertiseSelect"
-                                    class="form-select @error('expertise') is-invalid @enderror"
-                                    onchange="loadExpertisePrograms(this.value)">
-                                    <option value="">-- Pilih Bidang Keahlian --</option>
-                                    @foreach (array_keys($expertiseData) as $expertise)
-                                        <option value="{{ $expertise }}"
-                                            {{ old('expertise') == $expertise ? 'selected' : '' }}>
-                                            {{ $expertise == 'TIK' ? 'TIK (Teknologi Informasi dan Komunikasi)' : $expertise }}
-                                        </option>
-                                    @endforeach
-                                </select>
-                                @error('expertise')
-                                    <div class="invalid-feedback">{{ $message }}</div>
-                                @enderror
-                            </div>
-                            <div class="col-md-4">
-                                <label class="form-label">Program Keahlian</label>
-                                <select name="expertise_program" id="expertiseProgramSelect"
-                                    class="form-select @error('expertise_program') is-invalid @enderror" disabled
-                                    onchange="loadExpertiseConcentrations(this.value)">
-                                    <option value="">-- Pilih Program Keahlian --</option>
-                                </select>
-                                @error('expertise_program')
-                                    <div class="invalid-feedback">{{ $message }}</div>
-                                @enderror
-                            </div>
-                            <div class="col-md-4">
-                                <label class="form-label">Konsentrasi Keahlian</label>
-                                <select name="expertise_concentration" id="expertiseConcentrationSelect"
-                                    class="form-select @error('expertise_concentration') is-invalid @enderror" disabled
-                                    onchange="loadSaprasData(this.value)">
-                                    <option value="">-- Pilih Konsentrasi Keahlian --</option>
-                                </select>
-                                @error('expertise_concentration')
-                                    <div class="invalid-feedback">{{ $message }}</div>
-                                @enderror
-                            </div>
                         </div>
                     </div>
 
@@ -381,6 +342,8 @@
                             <i class="bi bi-journal-text"></i>
                             <strong>B - Data Sarana Prasarana (Sapras)</strong>
                         </div>
+
+                        @include('instrument.partials.v2.consentration')
 
                         {{-- Hidden input to store all sapras data --}}
                         <input type="hidden" name="answers[B.sapras]" id="sapras-data-input" value="{}">
